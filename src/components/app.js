fb537bc22db5d0c6a4ece810af9a5b3afb973a31
MichaelCapo23 <123pie23@gmail.com>
2019-01-26 19:00:30 -0800

    project setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../assets/css/app.scss';
-import logo from '../assets/images/logo.svg';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min'
+import {Route} from 'react-router-dom';
+import List from '../components/list'
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+    <div className={"container"}>
+        <Route exact path={"/"} component={List}/>
     </div>
 );
 
