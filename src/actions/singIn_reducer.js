d11dd51ebe102e1c9575c8a810143cbf57f8edaa
MichaelCapo23 <123pie23@gmail.com>
2019-01-27 19:00:21 -0800

    started on sign-in validation

---
+++ src/actions/singIn_reducer.js
@@ -0,0 +1,19 @@
+import types from './types';
+import axios from 'axios';
+
+export const signIn = values => dispatch => {
+    try {
+        axios.get('/api/singIn', values).then((token) => {
+            console.log("token: ", token);
+            localStorage.setItem("token", token.data.data);
+            dispatch({
+                type: types.SIGN_IN
+            })
+        })
+    } catch {
+        dispatch({
+            type: types.SIGN_IN_ERROR,
+            error: "invalid email and/or password"
+        })
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
+import {signIn} from '../actions/singIn_reducer'
 
 const rootReducer = combineReducers({
     form: formReducer,
+    signIn : signIn,
 });
 
 export default rootReducer;
