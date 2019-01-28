9d1a15a82a028aa5ecf228f02ad548bc3a726a6a
MichaelCapo23 <123pie23@gmail.com>
2019-01-27 16:41:07 -0800

    routing done all pages made, started on list page layout

---
+++ src/components/addStudent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Field, Form} from 'redux-form'
+
+class AddStudent extends  Component {
+    render() {
+        return(
+            <div>
+                <h1 className="center">Add Student</h1>
+            </div>
+        )
+    }
+}
+
+export default AddStudent
---
+++ src/components/signUp.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Field, Form} from 'redux-form';
+
+class SignUp extends Component {
+    render() {
+        return(
+            <div>SignUp</div>
+        )
+    }
+}
+
+export default SignUp
